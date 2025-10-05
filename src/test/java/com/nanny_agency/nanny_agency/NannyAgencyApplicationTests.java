7d7b020e8843d2677f617cea639b9fb6f917c7f0
Professor-Koodie <mafusenzo6@gmail.com>
2025-10-05 17:29:10 +0200

    nanny initial push

---
+++ src/test/java/com/nanny_agency/nanny_agency/NannyAgencyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nanny_agency.nanny_agency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NannyAgencyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
