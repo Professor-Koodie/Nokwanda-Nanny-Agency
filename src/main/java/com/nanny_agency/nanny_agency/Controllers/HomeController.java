c0e2eaf8f71fea543f5fa4dc2f11250140838024
Professor-Koodie <mafusenzo6@gmail.com>
2025-10-05 17:29:10 +0200

    nanny initial push

---
+++ src/main/java/com/nanny_agency/nanny_agency/Controllers/HomeController.java
@@ -0,0 +1,16 @@
+package com.nanny_agency.nanny_agency.Controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/nanny")
+    public String showHomePage(){
+        return "nanny_agency";
+    }
+
+}
---
+++ src/main/java/com/nanny_agency/nanny_agency/NannyAgencyApplication.java
@@ -0,0 +1,14 @@
+package com.nanny_agency.nanny_agency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NannyAgencyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NannyAgencyApplication.class, args);
+	}
+
+}
+
